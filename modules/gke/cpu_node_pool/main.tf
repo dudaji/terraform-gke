088c343ae44ecb69b9a950cba33c7fd9215d96e9
yoyota <smarthorse6@gmail.com>
2020-02-11 16:54:16 +0900

    Add g1 small pool

--- modules/gke/cpu_node_pool/main.tf
+++ modules/gke/cpu_node_pool/main.tf
@@ -12,6 +12,30 @@ variable "preemtible" {
   default = true
 }
 
+resource "google_container_node_pool" "g1-samll" {
+  name       = "g1-small"
+  cluster    = "${var.cluster}"
+  node_count = 0
+
+  autoscaling {
+    min_node_count = 0
+    max_node_count = "${var.max_node_count}"
+  }
+
+  node_config {
+    preemptible  = "${var.preemtible}"
+    disk_size_gb = "${var.disk_size_gb}"
+    machine_type = "g1-small"
+
+    metadata = "${map("disable-legacy-endpoints", "true")}"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
+
 resource "google_container_node_pool" "cpu-mem-1024" {
   count      = "${var.node_pool_count}"
   name       = "cpu-${pow(2, count.index)}-mem-${pow(2, count.index) * 1024}"
