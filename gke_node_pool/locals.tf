78e2ddf087309b7ff7c9a98172aaf9e010405f71
yoyota <smarthorse6@gmail.com>
2020-03-10 16:47:09 +0900

    Update node pool name

--- gke_node_pool/locals.tf
+++ gke_node_pool/locals.tf
@@ -5,7 +5,7 @@ locals {
   cpu                    = [for i in range(local.node_pool_count) : pow(2, var.cpu_start_exponent + (var.machine_type == "e2" ? i + 1 : i))]
   memory                 = [for i in range(local.node_pool_count) : (pow(2, i) + var.memory_start_exponent) * var.memory_coefficient]
   machine_type           = [for i in range(local.node_pool_count) : local.predifined_macine_type ? var.machine_type : format("%s-custom-%d-%d", var.machine_type, local.cpu[i], local.memory[i])]
-  name                   = [for i in range(local.node_pool_count) : local.predifined_macine_type ? var.machine_type : format("%s%s", local.name_gpu_prefix[i], local.machine_type[i])]
+  name                   = [for i in range(local.node_pool_count) : local.predifined_macine_type ? format("%s%s", var.preemtible ? "preemtible-" : "", var.machine_type) : format("%s%s", local.name_gpu_prefix[i], local.machine_type[i])]
   name_gpu_prefix        = [for i in range(local.node_pool_count) : local.gpu ? format("%s-%d-", var.gpu_type, local.gpu_count[i]) : ""]
   gpu_count              = [for i in range(local.node_pool_count) : local.gpu ? pow(2, i) : 0]
 }
