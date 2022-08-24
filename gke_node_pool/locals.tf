57c01a6ef3ea0fb9c8ea3536c8bdbaa8b999be65
YongTak Yoo <yongtaky@gmail.com>
2022-08-24 10:42:02 +0900

    Fix min to max when e2 cpu

--- gke_node_pool/locals.tf
+++ gke_node_pool/locals.tf
@@ -3,7 +3,7 @@ locals {
   predifined_macine_type  = length(var.machine_type) > 2
   name_preemptible_prefix = var.preemtible ? "pr-" : ""
   node_pool_count         = local.predifined_macine_type ? min(1, var.node_pool_count) : var.node_pool_count
-  cpu                     = [for i in range(local.node_pool_count) : pow(2, var.cpu_start_exponent + (var.machine_type == "e2" ? min(1, i) : i))]
+  cpu                     = [for i in range(local.node_pool_count) : pow(2, var.cpu_start_exponent + (var.machine_type == "e2" ? max(1, i) : i))]
   memory                  = [for i in range(local.node_pool_count) : (pow(2, i) + var.memory_start_exponent) * var.memory_coefficient]
   machine_type            = [for i in range(local.node_pool_count) : local.predifined_macine_type ? var.machine_type : format("%s-custom-%d-%d", var.machine_type, local.cpu[i], local.memory[i])]
   name                    = [for i in range(local.node_pool_count) : format("%s%s%s", local.name_preemptible_prefix, local.name_gpu_prefix[i], local.machine_type[i])]
