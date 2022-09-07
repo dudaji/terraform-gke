298ff8c5778f054c73a49ba2fa80bad9933a1b5b
YongTak Yoo <yongtaky@gmail.com>
2022-09-07 13:32:51 +0900

    Add oauth_scopes for gpu node pool

--- gke_node_pool/main.tf
+++ gke_node_pool/main.tf
@@ -16,6 +16,15 @@ resource "google_container_node_pool" "custom-node-pool" {
     disk_size_gb = var.disk_size_gb
     machine_type = local.machine_type[count.index]
 
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/trace.append"
+    ]
+
     guest_accelerator {
       type  = var.gpu_type
       count = local.gpu_count[count.index]
